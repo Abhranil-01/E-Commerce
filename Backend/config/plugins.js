f14a448e064aab7ec27a48922fd0a6081477e358
Abhranil-01 <abhranilkundu06@gmail.com>
2024-07-07 01:58:49 +0530

    commit

--- Backend/config/plugins.js
+++ Backend/config/plugins.js
@@ -16,66 +16,7 @@ module.exports = ({ env }) => ({
       },
     },
   
-    // Redis configuration
-    redis: {
-      config: {
-        connections: {
-          default: {
-            connection: {
-              host: '127.0.0.1',
-              port: 6379,
-              db: 0,
-            },
-            settings: {
-              debug: false,
-            },
-          },
-        },
-      },
-    },
-  
-    // Rest-cache configuration
-    "rest-cache": {
-      config: {
-        provider: {
-          name: "redis",
-          options: {
-            max: 32767,
-            connection: "default",
-          },
-        },
-        strategy: {
-          enableEtagSupport: true,
-          logs: true,
-          clearRelatedCache: true,
-          maxAge: 3600000,
-          contentTypes: [
-            // List of Content-Types UID to cache
-            "api::category.category",
-            "api::add-to-cart.add-to-cart",
-            "api::badminton.badminton",
-            "api::cricket.cricket",
-            "api::football.football",
-            "api::order-list.order-list",
-            "api::product.product",
-            "api::review.review",
-            "api::running.running",
-            "api::user-address.user-address",
+ 
 
-            {
-              contentType: "api::category.category",
-              maxAge: 3600000,
-              hitpass: false,
-              keys: {
-                useQueryParams: false,
-                useHeaders: ["accept-encoding"],
-              },
-              maxAge: 18000,
-              method: "GET",
-            },
-          ],
-        },
-      },
-    },
   });
   
